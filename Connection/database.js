f37f63b043d82bb643cbadd7494e5927b3986334
atri patel <atripatel2403@gmail.com>
2025-05-08 18:55:16 +0530

    login

--- Connection/database.js
+++ Connection/database.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const connection = async () => {   
     try {
-        await mongoose.connect('mongodb+srv://rathorerajveer854:3eWddTAVv7brG1e2@examportal.3gnq3ee.mongodb.net/' );
+        await mongoose.connect('mongodb+srv://rathorerajveer854:3eWddTAVv7brG1e2@examportal.3gnq3ee.mongodb.net/' ,{ 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true, 
+            serverSelectionTimeoutMS: 30000 
+          } );
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error);
